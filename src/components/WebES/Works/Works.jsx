3612b708f7779a450bce972fe2f019bf0b67b771
lKinox <reyanjimenez@gmail.com>
2022-05-26 11:55:34 -0400

    Remaster1

--- src/components/WebES/Works/Works.jsx
+++ src/components/WebES/Works/Works.jsx
@@ -3,9 +3,9 @@ import WorkVec from './WorkVec.svg'
 
 export function Works() {
     return (
-      <div className='WorksDAD' id='Works'>
+      <div className='WorksDAD'>
           <img src={WorkVec} alt=""></img>
-          <h1>Mis Trabajos:</h1>
+          <h1 id='Works'>Mis Trabajos:</h1>
           <div className='Works'>
             <WorkBoxes />
           </div>
